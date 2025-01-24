f9a966933b6f90833549f28088ac31c7afce7a73
Daniel Mansfeldt <daniel.mansfeldt@fh-kiel.de>
2025-01-24 10:58:01 +0000

    feat: add FeedforwardMLP class

--- src/clearshape/models/treelstm.py
+++ src/clearshape/models/treelstm.py
@@ -7,6 +7,7 @@ __author__ = "Max Borm, Max Brede"
 import dgl
 import torch
 import torch.nn as nn
+import torch.nn.functional as nnf
 
 class ChildSumTreeLSTMCell(nn.Module):
     def __init__(self, x_size, h_size):
@@ -110,3 +111,55 @@ class Linears(nn.Module):
         root_h = self.encoder(tree_batch)
         logits = self.linears(root_h)
         return logits
+
+class FeedforwardMLP(nn.Module):
+    """
+    A flexible feedforward multi-layer perceptron (MLP) implemented in PyTorch.
+
+    Parameters
+    ----------
+    input_dim : int
+        The number of input features.
+    hidden_layers : list of int
+        A list containing the number of units in each hidden layer.
+    output_dim : int
+        The number of output units.
+    activation : callable, optional
+        The activation function to use between layers (default is ReLU).
+    task_type : str, optional
+        The type of task, either 'classification' or 'regression'.
+    """
+
+    def __init__(self, input_dim, hidden_layers, output_dim, activation=nnf.relu, task_type='regression'):
+        super(FeedforwardMLP, self).__init__()
+        self.activation = activation
+        self.task_type = task_type
+        
+        # Define layers
+        layer_dims = [input_dim] + hidden_layers + [output_dim]
+        self.layers = nn.ModuleList([
+            nn.Linear(layer_dims[i], layer_dims[i + 1])
+            for i in range(len(layer_dims) - 1)
+        ])
+    
+    def forward(self, x):
+        """
+        Forward pass through the network.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor of shape (batch_size, input_dim).
+
+        Returns
+        -------
+        torch.Tensor
+            Output tensor of shape (batch_size, output_dim).
+        """
+        for layer in self.layers[:-1]:
+            x = self.activation(layer(x))
+        x = self.layers[-1](x)  # No activation on output layer
+        
+        if self.task_type == 'classification':
+            x = nnf.softmax(x, dim=1)
+        return x
