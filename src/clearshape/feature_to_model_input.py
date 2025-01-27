d2b0441add9497a28d2da96b595caf3252d4ac3b
Daniel Mansfeldt <daniel.mansfeldt@fh-kiel.de>
2025-01-27 10:10:58 +0000

    refactor: strip suffix from paths in master table
    ...and hence from the other data split tables.

--- src/clearshape/feature_to_model_input.py
+++ src/clearshape/feature_to_model_input.py
@@ -87,6 +87,7 @@ class FeatureModelInputPipeline:
         data = []
         for path in iter(source_folder.rglob("*.stp")):
             class_name = path.relative_to(source_folder).parent.as_posix()
+            relative_part_path = path.relative_to(source_folder).with_suffix("").as_posix()
             data.append((class_name, relative_part_path))
 
         self._master_table = pd.DataFrame(data, columns=["class", "path"])
