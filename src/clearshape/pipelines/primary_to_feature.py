a8c696ba241dbc4cf5be9b5a544c6294768b8182
Daniel Mansfeldt <daniel.mansfeldt@fh-kiel.de>
2025-06-12 08:47:55 +0000

    fix: set primary_to_feature pipeline to check for "step" instead of "stp"-files

--- src/clearshape/pipelines/primary_to_feature.py
+++ src/clearshape/pipelines/primary_to_feature.py
@@ -113,7 +113,7 @@ class PrimaryFeaturePipeline:
         )
         self._step_path_generator = (
             cons.PATHS.DATA_PRIMARY / "fabwave"
-        ).rglob("*.stp")
+        ).rglob("*.step")
         self._targets= []
         self._known_classes = []
 
@@ -313,7 +313,7 @@ class PrimaryFeaturePipeline:
         """
         # process all step files
         logger.info("Starting processing of step files.")
-        progress_bar = tqdm(desc="Files processed: ",total=len(list(Path(cons.PATHS.DATA_PRIMARY / "fabwave").rglob("*.stp"))), ncols=100)
+        progress_bar = tqdm(desc="Files processed: ",total=len(list(Path(cons.PATHS.DATA_PRIMARY / "fabwave").rglob("*.step"))), ncols=100)
 
         try:
             while True:
