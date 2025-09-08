eac6692bd4ad4210c027914d597bad97604d770d
Daniel Mansfeldt <daniel.mansfeldt@fh-kiel.de>
2025-09-08 11:50:38 +0200

    refactor: optimize tree-lstm for f1 score with macro averaging

--- src/clearshape/pipelines/model_input_to_models_tree_lstm.py
+++ src/clearshape/pipelines/model_input_to_models_tree_lstm.py
@@ -104,18 +104,18 @@ class TreeLSTMTuningPipeline():
         match self._current_stage:
             case "train":
                 return optuna.create_study(
-                    direction="maximize",
+                    direction="minimize",
                     pruner=optuna.pruners.MedianPruner(
                         n_startup_trials=3, n_warmup_steps=30
                     ),
                 )
             case "validation":
                 return optuna.create_study(
-                    direction="maximize",
+                    direction="minimize",
                 )
             case "test":
                 return optuna.create_study(
-                    direction="maximize",
+                    direction="minimize",
                 )
 
     def _load_scaler(self):
@@ -680,13 +680,10 @@ class TreeLSTMTuningPipeline():
         """
         match self._conf.test_metric:
             case "mean_squared_error":
-                logger.warning("MAKE SURE OPTIMIZATION FUNCTION IS *MINIMIZING* THE OBJECTIVE FUNCTION FOR MSE!!!")
                 return nn.MSELoss()
             case "cross_entropy":
-                logger.warning("MAKE SURE THE OPTIMIZATION FUNCTION IS MINIMIZING CROSS ENTROPY!!!")
                 return nn.CrossEntropyLoss()
             case "f1":
-                logger.warning("MAKE SURE THE OPTIMIZATION FUNCTION IS MAXIMIZING THE F1 SCORE!!!")
                 return MulticlassF1Score(num_classes=model.models[-1].layers[-2].out_features)
             case _:
                 raise ValueError(f"Test metric {self._conf.test_metric} not recognized.")
